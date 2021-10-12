662fadd772adde1bf638c676622c2afd97ccb1a6
Brian Duff <cairndubh@gmail.com>
2021-10-12 00:11:47 -0700

    Ok, this will probably work

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -63,7 +63,7 @@ app.use(express.static("../frontend/build"))
 // Silly workaround for heroku not handling react router properly.
 app.get("*", (_, res) => {
   var path = require('path')
-  res.sendFile(path.resolve(__dirname + "/../frontend/build/index.html"))
+  res.sendFile(path.resolve(__dirname + "/../../frontend/build/index.html"))
 })
 
 const port = process.env.PORT || 4000
